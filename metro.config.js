21f29adf8ff030fc37fecbe0528cab5c34b9cbb6
Jonny Allum <jonnyallum@gmail.com>
2025-12-14 18:22:45 +0000

    Provide local ws shim package

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,17 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const { resolve } = require('metro-resolver');
+const fs = require('fs');
 const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+
+const shimsPath = path.resolve(__dirname, 'shims');
+
+if (!fs.existsSync(shimsPath)) {
+  fs.mkdirSync(shimsPath, { recursive: true });
+}
+
+config.watchFolders = [...(config.watchFolders || []), shimsPath];
 
 config.resolver.extraNodeModules = {
   ...(config.resolver.extraNodeModules || {}),
@@ -14,7 +24,14 @@ config.resolver.extraNodeModules = {
   tls: path.resolve(__dirname, 'shims/tls.js'),
   url: path.resolve(__dirname, 'shims/url.js'),
   zlib: path.resolve(__dirname, 'shims/zlib.js'),
-  ws: path.resolve(__dirname, 'shims/ws.js'),
+};
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === 'stream' || moduleName.startsWith('stream/')) {
+    return { type: 'sourceFile', filePath: path.resolve(__dirname, 'shims/stream.js') };
+  }
+
+  return resolve(context, moduleName, platform);
 };
 
 module.exports = config;
